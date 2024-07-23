421743c74fad258948eda875e2fded5646600742
LTilio <Leandro.tilio@gmail.com>
2024-07-23 19:14:08 -0300

    Initial commit

---
+++ src/main/java/br/com/Desafio_i4/DesafioI4Application.java
@@ -0,0 +1,13 @@
+package br.com.Desafio_i4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioI4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioI4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Desafio_i4/DesafioI4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Desafio_i4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioI4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
